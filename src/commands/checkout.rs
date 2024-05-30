541ebfc491536044c53456ff205dd5e5162a2568
Lucas Torres <lucatsf@icloud.com>
2024-05-30 16:36:23 -0300

    Refactor checkout command to update HEAD file with branch reference

--- src/commands/checkout.rs
+++ src/commands/checkout.rs
@@ -1,11 +1,16 @@
 use std::fs;
 use crate::vcs::repository::get_repo_path;
-use crate::vcs::index::write_index;
 
 pub fn checkout(branch_name: &str) {
     let repo_path = get_repo_path();
     if !repo_path.exists() {
         eprintln!("Repositório não inicializado.");
+        return;
+    }
+
+    let head_path = repo_path.join("HEAD");
+    if !head_path.exists() {
+        eprintln!("Nenhum commit encontrado.");
         return;
     }
 
@@ -16,12 +21,17 @@ pub fn checkout(branch_name: &str) {
     }
 
     let commit_hash = fs::read_to_string(&branch_path).expect("Não foi possível ler a branch");
-    let head_path = repo_path.join("HEAD");
-    fs::write(head_path, commit_hash.clone()).expect("Não foi possível atualizar o HEAD");
+    let head_content = if branch_name == "master" {
+        format!("ref: refs/heads/master")
+    } else {
+        format!("ref: refs/heads/{}", branch_name)
+    };
 
-    // Resetando o índice para o estado do commit
-    let commit_tree = crate::vcs::object::read_commit_tree(&repo_path, commit_hash.trim());
-    write_index(&repo_path, &commit_tree);
+    fs::write(&head_path, head_content).expect("Não foi possível atualizar o HEAD");
+
+    // Atualizando o índice para o estado do commit
+    let commit_tree = crate::vcs::object::read_commit_tree(&repo_path, &commit_hash.trim());
+    crate::vcs::index::write_index(&repo_path, &commit_tree);
 
     println!("Mudado para a branch '{}'.", branch_name);
 }
